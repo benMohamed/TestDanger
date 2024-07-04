46a8d56b19e465dd297811b525b3253e0277cf0b
Mohamed Bendnaiba <mohamed.bendnaiba@vosker.com>
2024-07-04 06:38:51 +0100

    Initial Commit

---
+++ TestDanger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestDanger
+//
+//  Created by Mohammed on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestDanger/TestDangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDangerApp.swift
+//  TestDanger
+//
+//  Created by Mohammed on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
